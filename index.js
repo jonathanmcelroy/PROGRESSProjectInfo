6193385a85069ac46262f4f309f346a76dff50da
Jonathan McElroy <jonathanpmcelroy@gmail.com>
2015-07-01 10:26:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function getVariables() {
-  var contents = document.getElemenetById("text").value;
+  var contents = document.getElementById("text").value;
   var varDefRegex = /define\s+variable\s+((?:\w|-)+)/gi
 }
