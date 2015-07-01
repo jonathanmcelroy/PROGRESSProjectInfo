023a7508e975085e914fa148dfc4a0f0e78ef110
Jonathan McElroy <jonathanpmcelroy@gmail.com>
2015-07-01 10:42:25 -0500

    Added variable assignment and usage functions

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+function getContents() {
+  return document.getElementById("text").value;
+}
+
 function getVariables() {
-  var contents = document.getElementById("text").value;
-  var varDefRegex = /define\s+variable\s+((?:\w|-)+)/gi
+  var contents = getContents();
+  var varDefRegex = /define\s+variable\s+((?:\w|-)+)/gi;
+  
+  // TODO: this can be done functionally
+  var vars = [];
+  var result;
+  while((result = varDefRegex.exec(contents)) !== null) {
+    vars.add(result[1]);
+  }
+  return vars;
+}
+
+function testForAssignment(variable) {
+  var contents = getContents();
+  var assignmentRegex = new RegExp("(" + variable + "\s*=)|(output\s+" + variable + ")", "gi");
+  return (assignmentRegex.test(contents));
+}
+
+function testForUsage(variable) {
+  var contents = getContents();
+  var assignmentRegex = new RegExp("(=[^.]" + variable + ")|(input\s+" + variable + ")", "gi");
+  return (assignmentRegex.test(contents));
 }
