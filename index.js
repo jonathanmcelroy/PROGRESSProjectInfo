62ed2e8b6e198556e612257ede3238dce55a1f10
Jonathan McElroy <jonathanpmcelroy@gmail.com>
2015-07-02 10:23:26 -0500

    Add procedures

--- index.js
+++ index.js
@@ -38,3 +38,58 @@ function getAllUnusedOrUndefinedVariables() {
     }
   }
 }
+
+function getProcedureNamesAndIndices() {
+  var contents = getContents();
+  var procedureStart = /procedure +([^ ]+) +[.:]/gi
+  var procedureEnd   = /end procedure\./gi
+  
+  var vars = [];
+  var result;
+  while((result = procedureStart.exec(contents)) !== null) {
+    var startIndex = procedureStart.lastIndex;
+    procedureEnd.lastIndex = startIndex;
+    procedureEnd.exec(contents);
+    var endIndex = procedureEnd.lastIndex;
+    
+    vars.push([result[1], startIndex, endIndex]);
+  }
+  return vars;
+}
+
+function getProcedureCalls() {
+  var contents = getContents();
+  var regex = /run +([^ ]+) +[.:]/gi
+  
+  var vars = [];
+  var result;
+  while((result = regex.exec(contents)) !== null) {
+    vars.push([result[1], regex.lastIndex]);
+  }
+  return vars;
+}
+
+function getProcedureGraph() {
+  var contents = getContents();
+  var procedures = getProcedureNamesAndIndiced();
+  var dict = {};
+  for(var i=0; i<procedures.length; i++) {
+    var procedure = procedures[i];
+    dict[procedure = [];
+  }
+  
+  var calledProcedures = getProcedureCalls();
+  for(var i=0; i<calledProcedures.length; i++) {
+    var calledProcedure = calledProcedures[i];
+    var calledName = calledProcedure[0];
+    var index = calledProcedure[1];
+    
+    for(var j=0; j<procedures.length; j++) {
+      var procedure = procedures[j];
+      if(procedure[1] < index && index < procedure[2]) {
+        dict[procedure[0]].push(calledName);
+      }
+    }
+  }
+  return dict;
+}
