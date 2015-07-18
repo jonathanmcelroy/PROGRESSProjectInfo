d572f8f8bf441adf40211bda1428cf4b73f80278
Jonathan McElroy <jonathanpmcelroy@gmail.com>
2015-07-18 12:00:27 -0500

    Adding Node and Edge models to the graph app

--- PROGRESSGraphs/models.py
+++ PROGRESSGraphs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Node(models.Model):
+    TYPE_CHOICES = (
+        ("var", "Variable"),
+        ("proc", "Procedure"),
+        ("func", "Function"),
+    )
+    name = models.CharField()
+    progressType = models.CharField(max_length = 4,
+                                    choices = TYPE_CHOICES)
+    edges = models.ManyToManyField("self", through="Edge", symmetrical=False, related_name="Uses")
+
+class Edge(models.Model):
+    user = models.ForeignKey(Node, related_name="User")
+    used = models.ForeignKey(Node, related_name="Used")
