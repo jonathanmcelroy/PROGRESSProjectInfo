0148273a90a92dac239012fe0c01775ab4fb8678
Jonathan McElroy <jonathanpmcelroy@gmail.com>
2015-07-20 11:12:52 -0500

    Add file graph page

--- PROGRESSGraphs/urls.py
+++ PROGRESSGraphs/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^fileGraph$', views.fileGraph, name='fileGraph'),
     url(r'^procedureGraph$', views.procedureGraph, name='procedureGraph'),
 ]
--- PROGRESSGraphs/views.py
+++ PROGRESSGraphs/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
+def fileGraph(request):
+    return render(request, 'fileGraph.html', {})
+
 def procedureGraph(request):
     return render(request, 'procedureGraph.html', {})
